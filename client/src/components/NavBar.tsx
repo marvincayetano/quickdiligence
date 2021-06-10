d5f57aa0fe58a5537f669315af0fc3dab64f723d
Marvin Cayetano <marvin@Marvins-MacBook-Pro.local>
2021-06-09 20:49:33 -0400

    🎉 Chore: Nav logo

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import NextLink from "next/link";
 
-import Logo from "../images/logo.svg";
 import {
   Header,
   NavHeader,
-  Nav__container,
-  Nav__element,
   Nav__icon,
+  Nav__logo,
+  Nav__element,
+  Nav__container,
 } from "../styles/Header";
 
 interface NavBarProps {}
@@ -18,7 +18,13 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
       <NavHeader>
         <Nav__container>
           <Nav__element role="navigation">
-            <Nav__icon></Nav__icon>
+            <Nav__icon>
+              <NextLink href="/login">
+                <Nav__logo aria-current="page" target="_self" href="">
+                  <img src="/logo.svg" alt="quickdiligence logo" />
+                </Nav__logo>
+              </NextLink>
+            </Nav__icon>
           </Nav__element>
         </Nav__container>
       </NavHeader>
