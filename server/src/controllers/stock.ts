58696c942327587368454a2cf346a3c34a593a15
marvincayetano <cayetanomarvin@gmail.com>
2021-06-24 22:06:21 -0400

    🔥 Chore: EPS Scraping is done

--- server/src/controllers/stock.ts
+++ server/src/controllers/stock.ts
@@ -1,39 +1,82 @@
 import { Request, Response } from "express";
 import puppeteer from "puppeteer";
 
-const scrapePrice = async (symbol: string) => {
+export const getStockPrice = async (req: Request, res: Response) => {
   let browser = await puppeteer.launch();
   let page = await browser.newPage();
 
   try {
-    await page.goto(`https://ca.finance.yahoo.com/quote/${symbol}`, {
+    await page.goto(`https://ca.finance.yahoo.com/quote/${req.params.symbol}`, {
       waitUntil: "networkidle0",
     });
   } catch (err) {
     console.log(err);
   }
 
-  console.log("bout to page eval");
-  const currentPrice = async () =>
-    await page.evaluate(
+  page
+    .evaluate(
       () =>
         (
           document.querySelectorAll(
             "#quote-summary > div > table > tbody > tr > td > span"
           )[1] as HTMLElement
         )?.innerText
-    );
-
-  console.log("current price", await currentPrice());
+    )
+    .then((result) => {
+      res.send(result);
+    });
 };
 
-export const getStockPrice = (req: Request, res: Response) => {
-  console.log("PRICE", req.params);
-};
-
-export const getAnalyze = (req: Request, res: Response) => {
+export const getAnalyze = async (req: Request, res: Response) => {
   console.log("ANALYZE SYMBOL", req.params.symbol);
-  scrapePrice(req.params.symbol);
 
-  //   console.log("ANALYZE OPTIONS", req.query.currentOptions);
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
+
+  try {
+    await page.goto(
+      `https://ca.finance.yahoo.com/quote/${req.params.symbol}/analysis`,
+      {
+        waitUntil: "networkidle0",
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  const epsArr = await page.$$eval(
+    "table > tbody > tr:nth-child(5) > td",
+    (lists) => {
+      const contents = [];
+
+      for (var i = 1; i <= 4; i++) {
+        contents.push(lists[i].querySelector("span")?.innerText);
+      }
+
+      return contents;
+    }
+  );
+
+  console.log(epsArr);
+
+  //   const animals = await page.$$eval(".ResultsTable > tbody > tr", (lists) => {
+  //     // lists has all the values inside of td
+  //     // each list has 7 contents each
+  //     const contents = [];
+  //     for (var i = 1; i <= lists.length - 1; i++) {
+  //       contents.push({
+  //         imgLink: lists[i].querySelector("td:first-child > a > img").src,
+  //         animalType: lists[i].querySelector("td:nth-child(2)")?.innerText,
+  //         name: lists[i]
+  //           .querySelector("td:nth-child(3)")
+  //           ?.innerText.split("\n")[0],
+  //         gender: lists[i].querySelector("td:nth-child(4)")?.innerText,
+  //         color: lists[i].querySelector("td:nth-child(5)")?.innerText,
+  //         description: lists[i].querySelector("td:nth-child(6)")?.innerText,
+  //         age: lists[i].querySelector("td:last-child")?.innerText,
+  //         status: "found",
+  //       });
+  //     }
+  //     return contents;
+  //   });
 };
