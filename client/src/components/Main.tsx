47e46dbe4a3e56807a84b1e744cf0a78336198e2
marvincayetano <cayetanomarvin@gmail.com>
2021-07-03 03:48:36 -0400

    🎉 Feat: Added main page:

---
+++ client/src/components/Main.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import styled from "styled-components";
+import NextLink from "next/link";
+
+import { SearchIcon } from "@chakra-ui/icons";
+
+import { Nav__icon, Nav__logo, Nav__listItem } from "../styles/Header";
+
+interface MainProps {}
+
+export const Main: React.FC<MainProps> = ({}) => {
+  return (
+    <Main__container>
+      <Hero__container>
+        <Nav__icon>
+          <Nav__listItem>
+            <NextLink href="">
+              <Nav__logo aria-current="page" target="_self" href="">
+                <img src="/logo.svg" alt="quickdiligence logo" />
+              </Nav__logo>
+            </NextLink>
+          </Nav__listItem>
+        </Nav__icon>
+        <span color="gray.500">
+          A quick stock analyzer for your next 💎 hanzzz
+        </span>
+        <Main__searchInput>
+          <input
+            type="text"
+            spellCheck="false"
+            aria-autocomplete="list"
+            autoComplete="false"
+            placeholder="Search for the stock"
+          />
+          <div>
+            <SearchIcon />
+          </div>
+        </Main__searchInput>
+      </Hero__container>
+    </Main__container>
+  );
+};
+
+const Main__container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  width: 100vw;
+  letter-spacing: 0.5px;
+  font-weight: 400;
+`;
+
+const Hero__container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 2.2rem;
+`;
+
+const Main__searchInput = styled.div`
+  display: flex;
+  -webkit-box-align: stretch;
+  align-items: stretch;
+  position: relative;
+  color: #54575a;
+  width: 100%;
+  margin-top: 1rem;
+
+  input {
+    width: 100%;
+    height: 5rem;
+    padding-left: 5rem;
+    font-weight: 500;
+    outline: transparent solid 2px;
+    outline-offset: 2px;
+    background: #fff;
+    padding-inline-end: var(--chakra-space-4);
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+    box-shadow: var(--chakra-shadows-base);
+    border-radius: var(--chakra-radii-md);
+  }
+
+  div {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    position: absolute;
+    left: var(--chakra-space-7);
+    height: 5rem;
+    color: var(--chakra-colors-gray-400);
+  }
+
+  *,
+  ::before,
+  ::after {
+    border-color: var(--chakra-colors-gray-200);
+    overflow-wrap: break-word;
+  }
+  *,
+  ::before,
+  ::after {
+    border-width: 0px;
+    border-style: solid;
+    box-sizing: border-box;
+  }
+`;
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,14 +5,16 @@ import React, { useState } from "react";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { NavBar } from "../components/NavBar";
+import { Main } from "../components/Main";
 
 const MyApp = ({ Component, pageProps }: any) => {
   const [foundStock, setFoundStock] = useState({});
 
   return (
     <ChakraProvider>
-      <NavBar setFoundStock={setFoundStock} />
-      <Component {...pageProps} foundStock={foundStock} />
+      {/* <NavBar setFoundStock={setFoundStock} /> */}
+      <Main></Main>
+      {/* <Component {...pageProps} foundStock={foundStock} /> */}
     </ChakraProvider>
   );
 };
