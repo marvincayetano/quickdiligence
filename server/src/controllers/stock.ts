9d977c019cae364afc4e805ab1f59965d7cbfa71
marvincayetano <cayetanomarvin@gmail.com>
2021-07-05 02:02:36 -0400

    👍 Fix: C'est parfait cleanArray fn

--- server/src/controllers/stock.ts
+++ server/src/controllers/stock.ts
@@ -3,18 +3,24 @@ import puppeteer from "puppeteer";
 
 // To create a proper table for analysis
 const cleanArray = (elArr: string[], isBS: boolean = false) => {
-  console.log(elArr);
-  return elArr.map((block, index) => {
+  let result: any = {};
+
+  elArr.map((block, index) => {
     const colonAdded = block.replace(/\n|\t/g, ":");
     if (index === 0) {
       const dates = colonAdded
         .replace(`Breakdown:${isBS ? "" : "TTM"}`, "")
         .match(/.{1,10}/g) as string[];
-      return isBS ? ["Breakdown", ...dates] : ["Breakdown", "TTM", ...dates];
+      return isBS
+        ? (result["Date"] = [...dates])
+        : (result["Date"] = ["TTM", ...dates]);
     }
+    // The index 0 must be the object name and the rest are array of string
 
-    return colonAdded.split(/[:*]+/);
+    const split = colonAdded.split(/[:*]+/);
+    result[split[0]] = split.slice(1);
   });
+  return result;
 };
 
 // Analyze everything else here
@@ -32,47 +38,48 @@ export const getAnalyze = async () => {
   let els: any;
 
   // Stock Price
+  //   try {
+  //     await page.goto(`https://ca.finance.yahoo.com/quote/${req.params.symbol}`, {
+  //       waitUntil: "networkidle0",
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+
+  //   const stockPrice = await page.evaluate(
+  //     () =>
+  //       (
+  //         document.querySelectorAll(
+  //           "#quote-summary > div > table > tbody > tr > td > span"
+  //         )[1] as HTMLElement
+  //       )?.innerText
+  //   );
+
+  //   console.log(stockPrice);
+
+  // Income Statement
   try {
-    await page.goto(`https://ca.finance.yahoo.com/quote/${req.params.symbol}`, {
-      waitUntil: "networkidle0",
-    });
+    await page.goto(
+      `https://ca.finance.yahoo.com/quote/${req.params.symbol}/financials`,
+      {
+        waitUntil: "networkidle0",
+      }
+    );
   } catch (err) {
     console.log(err);
   }
 
-  const stockPrice = await page.evaluate(
-    () =>
-      (
-        document.querySelectorAll(
-          "#quote-summary > div > table > tbody > tr > td > span"
-        )[1] as HTMLElement
-      )?.innerText
+  const financials__incomeStatement = await page.$x(
+    "//div[contains(@class, 'D(tbr)')]"
+  );
+  els = await page.evaluate(
+    (...financials__incomeStatement) =>
+      financials__incomeStatement.map((e) => e.innerText),
+    ...financials__incomeStatement
   );
 
-  console.log(stockPrice);
-  //     // Income Statement
-  //     try {
-  //       await page.goto(
-  //         `https://ca.finance.yahoo.com/quote/${req.params.symbol}/financials`,
-  //         {
-  //           waitUntil: "networkidle0",
-  //         }
-  //       );
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-
-  //     const financials__incomeStatement = await page.$x(
-  //       "//div[contains(@class, 'D(tbr)')]"
-  //     );
-  //     els = await page.evaluate(
-  //       (...financials__incomeStatement) =>
-  //         financials__incomeStatement.map((e) => e.innerText),
-  //       ...financials__incomeStatement
-  //     );
-
-  //     const incomeStatementArr = cleanArray(els);
-  //     console.log("INCOME STATEMENT", incomeStatementArr);
+  const incomeStatementArr = cleanArray(els);
+  console.log("INCOME STATEMENT", incomeStatementArr);
 
   //   // Balance Sheet
   //   try {
@@ -121,77 +128,79 @@ export const getAnalyze = async () => {
 
   //   const cashFlowArr = cleanArray(els);
   //   console.log("INCOME STATEMENT", cashFlowArr);
-  //   res.send({
-  //     EPS: {
-  //       data: epsArr,
-  //       // Checks if the current EPS is positive
-  //       isPositiveNumber: epsArr[2] ? epsArr[2] > 0 : epsArr[0] > 0,
-  //       // Checks if the EPS is increasing each year
-  //       isIncreasing:
-  //         epsArr[0] < epsArr[1] && epsArr[1] < epsArr[2] && epsArr[2] < epsArr[3]
-  //           ? true
-  //           : false,
+
+  // res.send({
+  // console.log({
+  //   EPS: {
+  //     data: ,
+  //     // Checks if the current EPS is positive
+  //     isPositiveNumber: epsArr[2] ? epsArr[2] > 0 : epsArr[0] > 0,
+  //     // Checks if the EPS is increasing each year
+  //     isIncreasing:
+  //       epsArr[0] < epsArr[1] && epsArr[1] < epsArr[2] && epsArr[2] < epsArr[3]
+  //         ? true
+  //         : false,
+  //   },
+  //   PERatio: {
+  //     data: PERatio,
+  //     isUndervalued: PERatio < 14,
+  //     isOverValued: PERatio > 17,
+  //   },
+  //   IRatio: {
+  //     data: financials.ICRatio,
+  //     // Check if the IC ratio is 6 or higher
+  //     isSixHigher: financials.ICRatio > 6,
+  //     isOneToSix: financials.ICRatio < 6 && financials.ICRatio > 1,
+  //   },
+  //   RGrowth: {
+  //     data: financials.Revenue.reverse(),
+  //     // Check if Revenue is increasing
+  //     isIncreasing:
+  //       parseInt(financials.Revenue[0]) > parseInt(financials.Revenue[1]) &&
+  //       parseInt(financials.Revenue[1]) > parseInt(financials.Revenue[2])
+  //         ? true
+  //         : false,
+  //   },
+  //   IncomeLoss: {
+  //     data: financials.OILoss,
+  //     isNegative:
+  //       (financials.OILoss as any).charAt[1] &&
+  //       (financials.OILoss as any).charAt[0] === "-"
+  //         ? true
+  //         : false,
+  //   },
+  //   PnetIncome: {
+  //     data: financials.NIncome,
+  //     isPositive:
+  //       (financials.NIncome as any).charAt[1] &&
+  //       (financials.NIncome as any).charAt[0] === "-"
+  //         ? false
+  //         : true,
+  //   },
+  //   TotalCash: {
+  //     data: balanceSheet.TotalCash,
+  //   },
+  //   TotalAssets: {
+  //     data: {
+  //       assets: balanceSheet.TotalAssets,
+  //       liabilities: balanceSheet.TotalLiabilities,
   //     },
-  //     PERatio: {
-  //       data: PERatio,
-  //       isUndervalued: PERatio < 14,
-  //       isOverValued: PERatio > 17,
-  //     },
-  //     IRatio: {
-  //       data: financials.ICRatio,
-  //       // Check if the IC ratio is 6 or higher
-  //       isSixHigher: financials.ICRatio > 6,
-  //       isOneToSix: financials.ICRatio < 6 && financials.ICRatio > 1,
-  //     },
-  //     RGrowth: {
-  //       data: financials.Revenue.reverse(),
-  //       // Check if Revenue is increasing
-  //       isIncreasing:
-  //         parseInt(financials.Revenue[0]) > parseInt(financials.Revenue[1]) &&
-  //         parseInt(financials.Revenue[1]) > parseInt(financials.Revenue[2])
-  //           ? true
-  //           : false,
-  //     },
-  //     IncomeLoss: {
-  //       data: financials.OILoss,
-  //       isNegative:
-  //         (financials.OILoss as any).charAt[1] &&
-  //         (financials.OILoss as any).charAt[0] === "-"
-  //           ? true
-  //           : false,
-  //     },
-  //     PnetIncome: {
-  //       data: financials.NIncome,
-  //       isPositive:
-  //         (financials.NIncome as any).charAt[1] &&
-  //         (financials.NIncome as any).charAt[0] === "-"
-  //           ? false
-  //           : true,
-  //     },
-  //     TotalCash: {
-  //       data: balanceSheet.TotalCash,
-  //     },
-  //     TotalAssets: {
-  //       data: {
-  //         assets: balanceSheet.TotalAssets,
-  //         liabilities: balanceSheet.TotalLiabilities,
-  //       },
-  //       isPositiveAL:
-  //         parseFloat(balanceSheet.TotalAssets) >
-  //         parseFloat(balanceSheet.TotalLiabilities),
-  //     },
-  //     SHEquity: {
-  //       data: balanceSheet.SHEquity,
-  //       isIncreasing:
-  //         parseFloat(balanceSheet.SHEquity[0]) >
-  //           parseFloat(balanceSheet.SHEquity[1]) &&
-  //         parseFloat(balanceSheet.SHEquity[1]) >
-  //           parseFloat(balanceSheet.SHEquity[2])
-  //           ? true
-  //           : false,
-  //     },
-  //     LTD: {
-  //       data: balanceSheet.LongTermDebt,
-  //     },
-  //   });
+  //     isPositiveAL:
+  //       parseFloat(balanceSheet.TotalAssets) >
+  //       parseFloat(balanceSheet.TotalLiabilities),
+  //   },
+  //   SHEquity: {
+  //     data: balanceSheet.SHEquity,
+  //     isIncreasing:
+  //       parseFloat(balanceSheet.SHEquity[0]) >
+  //         parseFloat(balanceSheet.SHEquity[1]) &&
+  //       parseFloat(balanceSheet.SHEquity[1]) >
+  //         parseFloat(balanceSheet.SHEquity[2])
+  //         ? true
+  //         : false,
+  //   },
+  //   LTD: {
+  //     data: balanceSheet.LongTermDebt,
+  //   },
+  // });
 };
